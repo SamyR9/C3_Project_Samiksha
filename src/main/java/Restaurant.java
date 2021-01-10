58e02156478803afa3cf0c8ec803a8f3f2cbf30b
SamyR9”git config --global user.email samikshasinghrana@gmail.com” <samikshasinghrana@gmail.com>
2021-01-10 07:12:56 +0530

    Part2: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,15 +18,20 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        LocalTime currentTime = getCurrentTime();
+        if((currentTime.isAfter(openingTime) && currentTime.isBefore(closingTime)) || currentTime.equals(openingTime) || currentTime.equals(closingTime))
+            return true;
+        else
+            return false;
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        if (menu.size()>0)
+            return menu;
+        else
+            return null;
     }
 
     private Item findItemByName(String itemName){
@@ -41,7 +46,7 @@ public class Restaurant {
         Item newItem = new Item(name,price);
         menu.add(newItem);
     }
-    
+
     public void removeFromMenu(String itemName) throws itemNotFoundException {
 
         Item itemToBeRemoved = findItemByName(itemName);
